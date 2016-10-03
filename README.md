f5bf1d74bf8277cd3c4650b7932cd4119ac9eb46
Ivan <ivan@sq10.net>
2016-10-03 13:42:56 -0500

    code

---
+++ README.md
@@ -0,0 +1,15 @@
+guaranteed-error
+===
+
+[![npm version](https://img.shields.io/npm/v/guaranteed-error.svg?style=flat-square)](https://npmjs.com/package/guaranteed-error)
+[![javascript standard style](https://img.shields.io/badge/code%20style-standard-blue.svg?style=flat-square)](http://standardjs.com/)
+[![travis build](https://img.shields.io/travis/SEAPUNK/guaranteed-error/master.svg?style=flat-square)](https://travis-ci.org/SEAPUNK/guaranteed-error)
+[![coveralls coverage](https://img.shields.io/coveralls/SEAPUNK/guaranteed-error.svg?style=flat-square)](https://coveralls.io/github/SEAPUNK/guaranteed-error)
+[![david dependencies](https://david-dm.org/SEAPUNK/guaranteed-error.svg?style=flat-square)](https://david-dm.org/SEAPUNK/guaranteed-error)
+[![david dev dependencies](https://david-dm.org/SEAPUNK/guaranteed-error/dev-status.svg?style=flat-square)](https://david-dm.org/SEAPUNK/guaranteed-error)
+
+
+`npm install guaranteed-error`
+
+---
+
---
+++ test/index.js
@@ -0,0 +1,64 @@
+import test from 'ava'
+import guaranteedError from '../'
+
+test('empty errors', t => {
+  t.plan(3)
+
+  const fixed = guaranteedError()
+  t.is(fixed.name, 'Error')
+  t.is(fixed.message, '<No error message provided>')
+  t.is(fixed.stack,
+`Error: <No error message provided>
+    at <none>`
+  )
+})
+
+test('strings instead of errors', t => {
+  t.plan(3)
+
+  const fixed = guaranteedError('nice')
+  t.is(fixed.name, 'Error')
+  t.is(fixed.message, 'nice')
+  t.is(fixed.stack,
+`Error: nice
+    at <none>`
+  )
+})
+
+test('errors without names', t => {
+  t.plan(3)
+
+  const broken = new Error('okay')
+  broken.name = undefined
+
+  const fixed = guaranteedError(broken)
+  t.is(fixed.name, 'Error')
+  t.is(fixed.message, 'okay')
+  t.is(fixed.stack.indexOf('Error: okay'), 0)
+})
+
+test('errors without messages', t => {
+  t.plan(3)
+
+  const broken = new Error('okay')
+  broken.message = undefined
+
+  const fixed = guaranteedError(broken)
+  t.is(fixed.name, 'Error')
+  t.is(fixed.message, '<No error message provided>')
+  t.is(fixed.stack.indexOf('Error: <No error message provided>'), 0)
+})
+
+test('errors without stacks', t => {
+  t.plan(3)
+
+  const broken = new Error('okay')
+  broken.stack = undefined
+
+  const fixed = guaranteedError(broken)
+  t.is(fixed.name, 'Error')
+  t.is(fixed.message, 'okay')
+  t.is(fixed.stack,
+`Error: okay
+    at <none>`)
+})
